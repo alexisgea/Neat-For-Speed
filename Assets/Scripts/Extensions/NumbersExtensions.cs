9351d9af6c8bf52a6ae8523f30b71166b0c311c6
Alexis <alexis.giard@gmail.com>
2017-01-12 17:08:03 +0100

    - created some extensions for normalizing floats

---
+++ Assets/Scripts/Extensions/NumbersExtensions.cs
@@ -0,0 +1,12 @@
+﻿
+
+public static class NumberTypeExtensions {
+
+	public static float Normalized(this float thisFloat, float min = -1f, float max = 1f) {
+		return (thisFloat - min) / (max - min);
+	}
+
+	public static float DeNormalized(this float thisFloat, float min = -1f, float max = 1f) {
+		return (thisFloat * (max - min) - max);
+	}
+}
