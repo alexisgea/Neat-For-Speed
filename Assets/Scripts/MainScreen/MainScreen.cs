bfb6422d5a2f1a62731a8ca1bcb27bcb11c1f774
Alexis <alexis.giard@giard.com>
2017-04-06 18:10:52 +0200

    early main menu implementaiton

---
+++ Assets/Scripts/MainScreen/MainScreen.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public enum SceneName {MainScreen, TrainForCar}
+
+public class MainScreen : MonoBehaviour {
+
+    public static Dictionary<string, string> ScenePaths = new Dictionary<string, string>() {
+        {"MainScreen", "Scenes/MainScreen"},
+		{"TrainForCar", "Scenes/CarRace/ANNTraining"}
+    };
+
+    public static void GoToScene(SceneName scene) {
+        SceneManager.LoadScene(ScenePaths[scene.ToString()]);
+    }
+
+	public void LoadTrain() {
+        GoToScene(SceneName.TrainForCar);
+    }
+	
+	public void Quit() {
+        Application.Quit();
+    }
+}
