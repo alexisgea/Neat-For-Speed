962f6338d665ab2adb27575f2844938e49d683c7
Sébastien de Graffenried <seb.degraff@gmail.com>
2017-04-09 21:24:11 +0200

    The cells want food, now 🍰

---
+++ Assets/Cells/Scripts/Cell.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Cell : MonoBehaviour {
+	/// <summary>
+	/// Maximum force added in newton/second (I think :P )
+	/// </summary>
+	public float moveForwardForce = 2;
+
+	/// <summary>
+	/// Maximum rotation speed in degrees/seconds
+	/// </summary>
+	public float rotationSpeed = 180f;
+
+	public Collider2D cellCollider;
+	public ContactFilter2D foodFilter;
+
+	Rigidbody2D rb;
+	ICellInput input;
+
+	void Awake ()
+	{
+		rb = GetComponent<Rigidbody2D> ();
+		input = new PlayerCellInput ();
+	}
+			
+	void FixedUpdate ()
+	{
+		rb.AddRelativeForce (Vector2.up * moveForwardForce * input.GetMoveForward ());
+		rb.MoveRotation (rb.rotation + input.GetTurn () * -rotationSpeed * Time.deltaTime);
+
+		Eat ();
+	}
+
+	void Eat ()
+	{
+		print ("yo"); 
+		var results = new Collider2D[10];
+		int count = cellCollider.OverlapCollider (foodFilter, results);
+
+		for (int i = 0; i < count; i++) {
+			Destroy (results[i].gameObject);
+		}
+	}
+}
---
+++ Assets/Cells/Scripts/Environnement.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Environnement : MonoBehaviour {
+
+	public Transform foodPrefab;
+	public Transform foodParent;
+	public int foodInitialQuantity = 200;
+
+	void Start ()
+	{
+		Spawn ();
+	}
+
+	void Spawn ()
+	{
+		var bounds = GetComponent<Collider2D>().bounds;
+
+		for (int i = 0; i < foodInitialQuantity; i++)
+		{
+			var position = new Vector2 (Random.Range (bounds.min.x, bounds.max.x), Random.Range (bounds.min.y, bounds.max.y));
+			var food = Instantiate<Transform>(foodPrefab, foodParent);
+			food.position = position;
+			food.localScale = foodPrefab.localScale;
+		}
+	}
+}
